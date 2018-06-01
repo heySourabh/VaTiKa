7eabcdd706fe5a7e5653839fc7e029f0919519fa
Sourabh Bhat <heySourabh@gmail.com>
2018-06-01 12:47:01 +0530

    Minor refactoring

--- src/main/java/writer/DataFormat.java
+++ src/main/java/writer/DataFormat.java
@@ -3,11 +3,6 @@ package writer;
 public enum DataFormat {
     ASCII, BINARY;
 
-    public static void main(String[] args) {
-        System.out.println(ASCII);
-        System.out.println(BINARY);
-    }
-
     @Override
     public String toString() {
         return super.toString().toLowerCase();
